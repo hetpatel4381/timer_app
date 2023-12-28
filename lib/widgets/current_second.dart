5ea8d960283f519736462e2bcb8deb611b1781fb
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-12-28 23:07:50 +0530

    created folder struture for timer app

---
+++ lib/widgets/current_second.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CurrentSecondWidget extends StatelessWidget {
+  const CurrentSecondWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
