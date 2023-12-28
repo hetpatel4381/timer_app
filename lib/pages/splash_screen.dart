bd84c3c137a906f492cf66f8019f481b8074b9e5
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-12-28 23:18:34 +0530

    implemented splash and timer screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:timer_app/pages/timer_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const TimerScreen()),
+      );
+    });
+
     return const Scaffold(
       body: Center(
         child: FlutterLogo(size: 200),
--- lib/pages/timer_screen.dart
+++ lib/pages/timer_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:timer_app/widgets/button_widget.dart';
+import 'package:timer_app/widgets/circular_timer.dart';
+import 'package:timer_app/widgets/current_second.dart';
+import 'package:timer_app/widgets/random_number.dart';
+import 'package:timer_app/widgets/success_failure.dart';
 
 class TimerScreen extends StatelessWidget {
   const TimerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer App'),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: [
+          CurrentSecondWidget(),
+          RandomNumberWidget(),
+          SuccessFailureWidget(),
+          CircularTimerWidget(),
+          ButtonWidget(),
+        ],
+      ),
+    );
   }
 }
